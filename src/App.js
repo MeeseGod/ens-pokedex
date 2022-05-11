6b511fbddde22f949344cf9bf5137d58fb60723c
JosephR <joseph_rundell@outlook.com>
2022-05-11 15:59:05 -0400

    Updated routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import EnglishPokemon from "./components/EnglishPokemon"
 import JapanesePokemon from "./components/JapanesePokemon"
@@ -8,13 +8,13 @@ import JapanesePokemon from "./components/JapanesePokemon"
 export default function App(){
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" exact element={<Home/>} />
           <Route path="/japanese" exact element={<JapanesePokemon/>} />
           <Route path="/english" exact element={<EnglishPokemon/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
