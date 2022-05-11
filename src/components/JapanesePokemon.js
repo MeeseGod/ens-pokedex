00b78876cdc2f89708e3533994fb40328716770d
JosephR <joseph_rundell@outlook.com>
2022-05-11 14:04:54 -0400

    Added looksrare functionality

--- src/components/JapanesePokemon.js
+++ src/components/JapanesePokemon.js
@@ -41,6 +41,7 @@ export default function JapanesePokemon(){
             <th className="pokemonName">Name</th>
             <th className="ensLink" id="tableENS">ENS Link</th>
             <th className="osLink" id="tableOS">OS Link</th>
+            <th className="lrLink" id="tableLR">LR Link</th>
           </tr>
           {returnFilteredArray().map(pokemon => {
               return <tr key={pokemon} className="pokemonDisplay"> 
@@ -60,6 +61,12 @@ export default function JapanesePokemon(){
                   <a href={`https://opensea.io/assets/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/${GetOpenseaLink(pokemon)}`} className="osAnchor">
                   <img src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.png" className="osImage" alt="OpenSea's Logo"></img>
                   OS
+                </a>
+                </th>
+                <th className="lrLink">
+                  <a href={`https://looksrare.org/collections/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/${GetOpenseaLink(pokemon)}`} className="lrAnchor">
+                  <img src="../styles/looksrare.png" className="osImage" alt="Looks Rare's Logo"></img>
+                  LR
                 </a>
                 </th>
               </tr>
