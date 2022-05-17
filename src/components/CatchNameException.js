a3404efe3a623099bc6d1cd80c3cbbf8f56b736d
JosephR <joseph_rundell@outlook.com>
2022-05-17 15:45:45 -0400

    Testing functionality

--- src/components/CatchNameException.js
+++ src/components/CatchNameException.js
@@ -1,3 +1,4 @@
 export default function CatchNameException(name){
+        console.log(name.normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
         return name.normalize('NFD').replace(/[\u0300-\u036f]/g, '') // Remove accents
 }
